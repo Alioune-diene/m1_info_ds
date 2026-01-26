033ad8164d4e7d0851f379690416bd1e34775132
Alioune-diene <alioune.diene45@gmail.com>
2026-01-26 17:32:28 +0100

    Calculator : Fixed server side

--- lab1/calculator/ServerCalculator.java
+++ lab1/calculator/ServerCalculator.java
@@ -1,57 +1,62 @@
 
-
-
 import java.net.*;
 import java.io.*;
 
-
-
 public class ServerCalculator implements IServerCalculator {
+
     public static void main(String[] args) throws IOException {
-        
+
         if (args.length != 1) {
-            System.err.println("Usage: java EchoServer <port number>");
+            System.err.println("Usage: java ServerCalculator <port number>");
             System.exit(1);
         }
-        
+
         int portNumber = Integer.parseInt(args[0]);
-        
+        ServerCalculator calculator = new ServerCalculator();
+
         try (
-            ServerSocket serverSocket =
-                new ServerSocket(Integer.parseInt(args[0]));
-            Socket clientSocket = serverSocket.accept();     
-            PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);                   
-            DataInputStream in = new DataInputStream(
-                clientSocket.getInputStream())
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream())
         ) {
-            Integer op;
-            
-            while (( op = in.readInt()) != null) {
-                  Integer a, b, result;    
-                  a = in.readInt();
-                  b = in.readInt();
-                
-                switch (op) {
-                     case EOperationType.PLUS:
-                        result = plus(a, b);
-                         break;
+            System.out.println("Client connected!");
 
-                     case MINUS:
-                         result = minus(a, b);
-                         break;
+            while (true) {
+                try {
+                    int op = in.readInt();
+                    int a = in.readInt();
+                    int b = in.readInt();
+                    int result;
 
-                    case  DIVIDE:
-                         result = divide(a, b);
-                         break;
+                    EOperationType operation = EOperationType.values()[op];
 
-                    case MULTIPLY:
-                         result = multiply(a, b);
-                         break;
-                 }
-                out.println(result);
+                    switch (operation) {
+                        case PLUS:
+                            result = calculator.plus(a, b);
+                            break;
+                        case MINUS:
+                            result = calculator.minus(a, b);
+                            break;
+                        case DIVIDE:
+                            result = calculator.divide(a, b);
+                            break;
+                        case MULTIPLY:
+                            result = calculator.multiply(a, b);
+                            break;
+                        default:
+                            result = 0;
+                    }
+
+                    out.writeInt(result);
+                    out.flush();
+
+                } catch (EOFException e) {
+                    break;
+                }
             }
-            System.out.println("I'm ending...");
+            System.out.println("Client disconnected.");
+
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
                 + portNumber + " or listening for a connection");
@@ -59,12 +64,15 @@ public class ServerCalculator implements IServerCalculator {
         }
     }
 
+    @Override
+    public int plus(int a, int b) { return a + b; }
 
-    public int plus(int a , int b) { return a + b; }
-    public int minus (int a, int b) { return a - b; }
+    @Override
+    public int minus(int a, int b) { return a - b; }
+
+    @Override
     public int divide(int a, int b) { return a / b; }
+
+    @Override
     public int multiply(int a, int b) { return a * b; }
-
-
-
 }
