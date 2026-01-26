a24d36dae574a4093fed83a05ded75b6aca685d5
antoine <antoine.patron@etu.univ-littoral.fr>
2026-01-26 19:49:56 +0100

    PersonRegistry

---
+++ src/main/java/fr/uga/im2ag/m1info/lab1/phoneRegistry/EFunction.java
@@ -0,0 +1,9 @@
+package fr.uga.im2ag.m1info.lab1.phoneRegistry;
+
+public enum EFunction {
+    ADD,
+    GET_PHONE,
+    GET_ALL,
+    SEARCH,
+    NULL
+}
---
+++ src/main/java/fr/uga/im2ag/m1info/lab1/phoneRegistry/EResult.java
@@ -0,0 +1,10 @@
+package fr.uga.im2ag.m1info.lab1.phoneRegistry;
+
+public enum EResult {
+    SUCCESS,
+    FAILURE,
+    NOT_FOUND,
+    START_OF_LIST,
+    END_OF_LIST,
+    UNKNOWN_FUNCTION
+}
