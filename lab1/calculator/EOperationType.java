332cbd56a6639f786777985d860510e695a2e973
Alioune-diene <alioune.diene45@gmail.com>
2026-01-26 16:39:49 +0100

    Calculator : re-structure the code

---
+++ lab1/calculator/EOperationType.java
@@ -0,0 +1,6 @@
+public enum EOperationType {
+    PLUS,
+    MINUS,
+    DIVIDE,
+    MULTIPLY
+}
--- lab1/calculator/server/server_interface.java
+++
@@ -1,6 +0,0 @@
-interface Calculator_itf {
-    public int plus(int, int);
-    public int minus (int, int);
-    public int divide(int, int);
-    public int multiply(int, int);
-}
