d543d9bf95b31a34777afb499db0b698dc6db08e
Alioune-diene <alioune.diene45@gmail.com>
2026-03-09 14:51:38 +0100

    Rewritten code in java
    Passed from python to java for the RabbitMQ tutorials.

---
+++ src/main/java/fr/uga/im2ag/m1info/rabbbitmq/publish_subscrive/RecLogs.java
@@ -0,0 +1,33 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+public class RecLogs {
+
+    private final static String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        System.out.println(" [*] Waiting for logs. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] " + message);
+        };
+
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
+    }
+}
---
+++ src/main/java/fr/uga/im2ag/m1info/rabbbitmq/work_queues/NewTask.java
@@ -0,0 +1,26 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.nio.charset.StandardCharsets;
+
+public class NewTask {
+
+    private final static String QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            String message = argv.length > 0 ? String.join(" ", argv) : "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
